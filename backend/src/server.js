8050efa2c3631d8142fbde66c20436c7a3e0fd5d
bharath391 <bharathkumarmerigala@gmail.com>
2025-07-10 14:19:52 +0530

    global package for deployement

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import path from "path";
-
+import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js";
@@ -13,7 +13,8 @@ import { connectDB } from "./lib/db.js";
 const app = express();
 const PORT = process.env.PORT;
 
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(
   cors({
@@ -28,6 +29,14 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
+
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+app.use("*",(req,res) =>{
+  console.log('searching path ',path);
+  res.sendFile(path.join(__dirname,"../../frontend/dist/index.html"))
+  return
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
